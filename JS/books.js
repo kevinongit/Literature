8b3bebc669a539924ec66cf0bf421be571a77bbf
Kevin <nait90@gmail.com>
2022-10-25 13:57:55 +0900

    Update books.js

--- JS/books.js
+++ JS/books.js
@@ -1,1 +1,6 @@
-
+let books = [
+  { title: "Composing Software",
+    author: "Eric Elliot",
+    link: "https://www.amazon.com/Composing-Software-Exploration-Programming-Composition/dp/1661212565",
+  },
+]
